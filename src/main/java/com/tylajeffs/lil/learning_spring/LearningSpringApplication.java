80a2455ee058d7282dcc132755aa35688f920e3c
Tyla Jeffs <tylajeffs@gmail.com>
2024-09-25 09:44:50 -0400

    initialize springboot project

---
+++ src/main/java/com/tylajeffs/lil/learning_spring/LearningSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tylajeffs.lil.learning_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tylajeffs/lil/learning_spring/LearningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tylajeffs.lil.learning_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
